acf2439776444c7db386629cb1dfed6cc680e172
Taylor Phillips <taylorscottphillips@gmail.com>
2022-12-17 00:29:00 -0600

    Update nvim plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -24,13 +24,8 @@ return require('packer').startup(function(use)
       {'hrsh7th/nvim-cmp'},
       {'hrsh7th/cmp-buffer'},
       {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
       {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      {'hrsh7th/cmp-nvim-lua'}
     }
   }
 

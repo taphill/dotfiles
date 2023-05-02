572c66d7661a0e18c6508065b9ae803eedc715b0
Taylor Phillips <taylorscottphillips@gmail.com>
2023-05-01 20:11:59 -0500

    Update nvim lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,83 +3,144 @@
 -- See `:help lsp-zero`
 --
 
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = false,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
+local lsp = require("lsp-zero").preset({
+	name = "minimal",
+	set_lsp_keymaps = false,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = false,
 })
 
 lsp.ensure_installed({
-  'denols',
-  'prismals',
-  'tailwindcss',
-  'tsserver',
+	"denols",
+	"prismals",
+	"tailwindcss",
+	"tsserver",
+	"yamlls",
 })
 
-local lsp_util = require('lspconfig.util')
+local lsp_util = require("lspconfig.util")
 
-lsp.configure('denols', {
-  root_dir = lsp_util.root_pattern('deno.json','deno.jsonc')
+lsp.configure("denols", {
+	root_dir = lsp_util.root_pattern("deno.json", "deno.jsonc"),
 })
 
-lsp.configure('tailwindcss', {
-  root_dir = lsp_util.root_pattern('tailwind.config.js', 'tailwind.config.cjs')
+lsp.configure("tailwindcss", {
+	root_dir = lsp_util.root_pattern("tailwind.config.js", "tailwind.config.cjs"),
 })
 
-lsp.configure('tsserver', {
-  root_dir = lsp_util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json')
+lsp.configure("tsserver", {
+	root_dir = lsp_util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
 })
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+lsp.configure("yamlls", {
+	settings = {
+		yaml = {
+			customTags = {
+				"!Base64",
+				"!Cidr",
+				"!FindInMap sequence",
+				"!GetAtt",
+				"!GetAZs",
+				"!ImportValue",
+				"!Join sequence",
+				"!Ref",
+				"!Select sequence",
+				"!Split sequence",
+				"!Sub sequence",
+				"!Sub",
+				"!And sequence",
+				"!Condition",
+				"!Equals sequence",
+				"!If sequence",
+				"!Not sequence",
+				"!Or sequence",
+			},
+			schemas = {
+				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+				"https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json",
+				"https://raw.githubusercontent.com/aws/serverless-application-model/main/samtranslator/schema/schema.json",
+			},
+		},
+	},
+})
+
+local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+null_ls.setup({
+	-- Diagnostics for various Linters and formatters
+	sources = {
+		null_ls.builtins.diagnostics.cfn_lint,
+		null_ls.builtins.diagnostics.yamllint,
+		null_ls.builtins.formatting.deno_fmt,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.prismaFmt,
+		null_ls.builtins.formatting.stylua,
+	},
+
+	-- Format on save
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
+
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
 
 -- disable completion with tab
 -- this helps with copilot setup
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+	mapping = cmp_mappings,
 })
 
 lsp.set_preferences({
-  suggest_lsp_servers = false,
-  sign_icons = {
-    error = 'E',
-    warn = 'W',
-    hint = 'H',
-    info = 'I'
-  }
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = "E",
+		warn = "W",
+		hint = "H",
+		info = "I",
+	},
 })
 
 vim.diagnostic.config({
-  virtual_text = true,
+	virtual_text = true,
 })
 
 lsp.on_attach(function(client, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
 
-  nmap(']d', vim.diagnostic.goto_next, 'Go to next Diagnostic')
-  nmap('[d', vim.diagnostic.goto_prev, 'Go to previous Diagnostic')
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+	nmap("]d", vim.diagnostic.goto_next, "Go to next Diagnostic")
+	nmap("[d", vim.diagnostic.goto_prev, "Go to previous Diagnostic")
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 end)
 
 lsp.setup()
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,74 +1,76 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- LSP
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+	-- LSP
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "jose-elias-alvarez/null-ls.nvim" },
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
-    }
-  }
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 
-  -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-treesitter/playground' }
+	-- Treesitter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "nvim-treesitter/playground" })
 
-  -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+	-- Fuzzy Finder (files, lsp, etc)
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { "nvim-lua/plenary.nvim" } })
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-  -- Copilot
-  use 'github/copilot.vim'
+	-- Copilot
+	use("github/copilot.vim")
 
-  -- Statusline
-  use 'nvim-lualine/lualine.nvim'
+	-- Statusline
+	use("nvim-lualine/lualine.nvim")
 
-  -- Colorscheme
-  use 'rebelot/kanagawa.nvim'
+	-- Colorscheme
+	use("rebelot/kanagawa.nvim")
+	use("folke/tokyonight.nvim")
+	use("MaxMEllon/vim-jsx-pretty")
 
-  -- Utilities
-  use 'mattn/emmet-vim'
-  use 'mbbill/undotree'
-  use 'windwp/nvim-autopairs'
-  use 'prettier/vim-prettier'
-  use 'MaxMEllon/vim-jsx-pretty'
-  use 'lewis6991/impatient.nvim'
-  use 'christoomey/vim-tmux-navigator'
-  use 'christoomey/vim-tmux-runner'
-  use 'architect/vim-plugin'
-  -- use '~/dev/projects/vim-plugin'
+	-- Utilities
+	use("mattn/emmet-vim")
+	use("mbbill/undotree")
+	use("windwp/nvim-autopairs")
+	use("prettier/vim-prettier")
+	use("lewis6991/impatient.nvim")
+	use("christoomey/vim-tmux-navigator")
+	use("christoomey/vim-tmux-runner")
+	use("architect/vim-plugin")
+	-- use '~/dev/projects/vim-plugin'
 
-  -- Text objects
-  use 'wellle/targets.vim'
-  use 'kana/vim-textobj-line'
-  use 'kana/vim-textobj-user'
-  use 'kana/vim-textobj-entire'
-  use 'michaeljsmith/vim-indent-object'
+	-- Text objects
+	use("wellle/targets.vim")
+	use("kana/vim-textobj-line")
+	use("kana/vim-textobj-user")
+	use("kana/vim-textobj-entire")
+	use("michaeljsmith/vim-indent-object")
 
-  -- Tpope stuff
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-endwise'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rails'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-unimpaired'
-  use 'tpope/vim-vinegar'
+	-- Tpope stuff
+	use("tpope/vim-commentary")
+	use("tpope/vim-endwise")
+	use("tpope/vim-fugitive")
+	use("tpope/vim-rails")
+	use("tpope/vim-repeat")
+	use("tpope/vim-surround")
+	use("tpope/vim-unimpaired")
+	use("tpope/vim-vinegar")
 end)

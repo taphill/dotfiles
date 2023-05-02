d3838bb8b1307b744ef2f162a269134c24358680
Taylor Phillips <taylorscottphillips@gmail.com>
2023-05-01 21:34:01 -0500

    Update nvim colorscheme

--- .config/nvim/after/plugin/colorscheme.lua
+++ .config/nvim/after/plugin/colorscheme.lua
@@ -4,6 +4,41 @@
 
 require("tokyonight").setup({
 	style = "moon",
+	on_highlights = function(hl, c)
+		hl.PreProc = {
+			fg = c.red,
+		}
+
+		-- Make Telescope borderless
+		local prompt = "#2d3149"
+		hl.TelescopeNormal = {
+			bg = c.bg_dark,
+			fg = c.fg_dark,
+		}
+		hl.TelescopeBorder = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopePromptNormal = {
+			bg = prompt,
+		}
+		hl.TelescopePromptBorder = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePromptTitle = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePreviewTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopeResultsTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+	end,
 })
 
 vim.cmd("colorscheme tokyonight")
